dd617ecc70288a11594e48bde9db76ab756d65d5
Administrator <Administrator@USER-4ICR6FMTU6>
2017-12-22 19:29:29 +0800

    shi

---
+++ src/main/java/dt59homework/work.java
@@ -0,0 +1,26 @@
+/**
+ * Project Name:dt59homework
+ * File Name:work.java
+ * Package Name:dt59homework
+ * Date:2017年12月22日下午7:22:40
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package dt59homework;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月22日 下午7:22:40 <br/>
+ * @author   Administrator
+ * @version
+ * @see
+ */
+public class work {
+
+    public static void main(String[] args) {
+
+        //  Auto-generated method stub
+         System.out.println("1");
+    }
+
+}
+
